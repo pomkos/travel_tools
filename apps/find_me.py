1192e6c3451067db147a79c92c9cdec3c7ee8390
pomkos <albeithome@gmail.com>
2021-02-22 03:38:00 +0000

    fleshed out find me

--- apps/find_me.py
+++ apps/find_me.py
@@ -9,10 +9,17 @@ def settings():
     
     with col1:
         current_loc = st.text_input("Where are you?")
+        my_pic = st.file_uploader("Upload plane ticket",type=['png','jpg'])
+        
+        if my_pic is not None:
+            st.write(uploaded_file.getvalue())
+        
         current_date = dt.datetime.now()
     with col2:
         future_loc = st.text_input("Where are you going?")
         future_date = st.date_input("When?", min_value=dt.datetime.now())
+        
+
 
 def app():
     st.title("Where in the world is Peter Gates?")
