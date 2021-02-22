0de2887d45aad976d5df7325b50adfe66058a644
pomkos <albeithome@gmail.com>
2021-02-22 03:01:00 +0000

    find pete

---
+++ apps/find_me.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def app():
+    st.title("Where in the world is Peter Gates?")
+    
+
+app()
--- travel_tools.py
+++ travel_tools.py
@@ -2,13 +2,17 @@ import streamlit as st
 
 st.set_page_config(page_title='Traveling Tuesday', page_icon="✈️")
 
-selection = st.sidebar.selectbox("What should we do", ['Home','Review'])
+selection = st.sidebar.selectbox("What should we do", ['Find Peter','Research','Review'])
 selection = selection.lower()
 
 if 'home' in selection:
     from apps import explore
     explore.app()
     
-elif 'review' in selection:
+elif 'research' in selection:
     from apps import review
     review.app()
+    
+elif 'peter' in selection:
+    from apps import find_me
+    find_me.app()
