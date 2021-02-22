45ce4cc79b21e5e798ecb796e03b6888e689a8a7
pomkos <albeithome@gmail.com>
2021-02-22 03:24:51 +0000

    fleshed out find me

--- apps/find_me.py
+++ apps/find_me.py
@@ -5,11 +5,11 @@ import streamlit as st
 def app():
     st.title("Where in the world is Peter Gates?")
     st.write("""
-    Right now he is in __Mexico City__
-    He will be in __Naples, FL__ on Feb 28
+    * Right now he is in __Mexico City__
+    * He will be in __Naples, FL__ on Feb 28
     """)
     st.write("## Plane Ticket")
-    st.image("images/ticket.png")
+    #st.image("images/ticket.png")
     
     # admin page
     admin = st.experimental_get_query_params()
